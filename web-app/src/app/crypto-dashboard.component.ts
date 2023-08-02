932f92ed8b8cca55959ad41b1d83fd3a55d14300
Douglas Craici <douglas.craici@norgelabs.com>
2023-08-02 06:28:43 -0300

    creating view component myCrypto

---
+++ web-app/src/app/crypto-dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-crypto-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  template: ``,
+  styles: [``]
+})
+export class CryptoDashboardComponent {
+
+}
