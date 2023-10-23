04a7502be024523b3ca5bc4d9157e4ffae398c97
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2023-10-23 22:57:42 +0100

    addinf EF Core ProposalConfiguration

--- api/api/Data/DataContext.cs
+++ api/api/Data/DataContext.cs
@@ -18,32 +18,6 @@ public class DataContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Proposal>().OwnsOne(x => x.Customer, c =>
-         {
-             c.Property(x => x.Email).HasColumnType("varchar").HasMaxLength(255);
-             c.Property(x => x.Name).HasColumnType("varchar").HasMaxLength(255);
-             c.Property(x => x.Phone).HasColumnType("varchar").HasMaxLength(255);
-         });
-
-        modelBuilder.Entity<Proposal>().OwnsOne(x => x.Address, a =>
-        {
-            a.Property(x => x.Street).HasColumnType("varchar").HasMaxLength(255);
-            a.Property(x => x.Number).HasColumnType("varchar").HasMaxLength(255);
-            a.Property(x => x.City).HasColumnType("varchar").HasMaxLength(255);
-            a.Property(x => x.State).HasColumnType("varchar").HasMaxLength(255);
-            a.Property(x => x.ZipCode).HasColumnType("varchar").HasMaxLength(255);
-        });
-
-        modelBuilder.Entity<Proposal>().OwnsMany(x => x.Products, p =>
-        {
-            p.Property(x => x.Name).HasColumnType("varchar").HasMaxLength(255);
-        });
-
-        modelBuilder.Entity<Proposal>().Property(x => x.Notes).HasColumnType("varchar").HasMaxLength(255);
-        modelBuilder.Entity<Proposal>().Property(x => x.Power).HasColumnType("varchar").HasMaxLength(255);
-        modelBuilder.Entity<Proposal>().Property(x => x.TotalPrice).HasPrecision(18, 8);
-        modelBuilder.Entity<Proposal>().Property(x => x.TotalPriceProducts).HasPrecision(18, 8);
-        modelBuilder.Entity<Proposal>().Property(x => x.LabourValue).HasPrecision(18, 8);
     }
 
 }
