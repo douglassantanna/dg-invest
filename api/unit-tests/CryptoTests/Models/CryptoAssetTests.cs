4c141da6f6e79b3129dfd8a0cc23446b46548287
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2023-11-14 23:44:40 +0000

    added test 'GetPercentDifference_GivenCurrentPrice_ShouldReturnExpectedPercentDifference'
    in addition, added static method 'AddTransactions'

--- api/unit-tests/CryptoTests/Models/CryptoAssetTests.cs
+++ api/unit-tests/CryptoTests/Models/CryptoAssetTests.cs
@@ -36,24 +36,7 @@ public class CryptoAssetTests
         var cryptoAsset = _validCryptoAsset;
 
         // Act
-        List<CryptoTransaction> transactions = new()
-        {
-            new(amount: 1,
-                price: price1,
-                purchaseDate: DateTimeOffset.Parse("2023-10-10"),
-                exchangeName: "Binance",
-                transactionType: ETransactionType.Buy),
-            new(amount: 1,
-                price: price2,
-                purchaseDate: DateTimeOffset.Parse("2023-10-10"),
-                exchangeName: "Binance",
-                transactionType: ETransactionType.Buy)
-        };
-
-        foreach (var transaction in transactions)
-        {
-            cryptoAsset.AddTransaction(transaction);
-        }
+        AddTransactions(price1, price2, cryptoAsset);
 
         // Assert
         cryptoAsset.TotalInvested.Should().Be(expectedTotalCost);
@@ -93,5 +76,48 @@ public class CryptoAssetTests
 
         // Assert
         cryptoAsset.Balance.Should().Be(0);
+    }
+    [Theory]
+    [InlineData(10, 15, 15, 20)]
+    [InlineData(2, 5, 35, 900)]
+    public void GetPercentDifference_GivenCurrentPrice_ShouldReturnExpectedPercentDifference(
+        decimal price1,
+        decimal price2,
+        decimal currentPrice,
+        decimal expectedPercentDifference)
+    {
+        // Arrange
+        var cryptoAsset = _validCryptoAsset;
+        AddTransactions(price1, price2, cryptoAsset);
+
+        // Act
+        var result = cryptoAsset.GetPercentDifference(currentPrice);
+
+        // Assert
+        result.Should().Be(expectedPercentDifference);
+    }
+
+    private static void AddTransactions(decimal price1,
+                                        decimal price2,
+                                        CryptoAsset cryptoAsset)
+    {
+        List<CryptoTransaction> transactions = new()
+        {
+            new(amount: 1,
+                price: price1,
+                purchaseDate: DateTimeOffset.Parse("2023-10-10"),
+                exchangeName: "Binance",
+                transactionType: ETransactionType.Buy),
+            new(amount: 1,
+                price: price2,
+                purchaseDate: DateTimeOffset.Parse("2023-10-10"),
+                exchangeName: "Binance",
+                transactionType: ETransactionType.Buy)
+        };
+
+        foreach (var transaction in transactions)
+        {
+            cryptoAsset.AddTransaction(transaction);
+        }
     }
 }
