b84f71bed8b17337e6c1839e7875910f428ff289
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2023-11-14 22:46:42 +0000

    added test 'CryptoAsset_WhenAddedBuyTransactions_TotalInvestedShouldBeEqualsToSumOfTransactionsPrice'

--- api/unit-tests/CryptoTests/Models/CryptoAssetTests.cs
+++ api/unit-tests/CryptoTests/Models/CryptoAssetTests.cs
@@ -1,3 +1,4 @@
+using System.Transactions;
 using api.Cryptos.Models;
 using api.Models.Cryptos;
 using FluentAssertions;
@@ -23,5 +24,37 @@ public class CryptoAssetTests
 
         // Assert
         cryptoAsset.Addresses.Should().HaveCount(1);
+    }
+
+    [Theory]
+    [InlineData(10, 20, 30)]
+    [InlineData(48, 14, 62)]
+    public void CryptoAsset_WhenAddedBuyTransactions_TotalInvestedShouldBeEqualsToSumOfTransactionsPrice(decimal price1, decimal price2, decimal expectedTotalCost)
+    {
+        // Arrange
+        var cryptoAsset = _validCryptoAsset;
+
+        // Act
+        List<CryptoTransaction> transactions = new List<CryptoTransaction>
+        {
+            new(amount: 1,
+                price: price1,
+                purchaseDate: DateTimeOffset.Parse("2023-10-10"),
+                exchangeName: "Binance",
+                transactionType: ETransactionType.Buy),
+            new(amount: 1,
+                price: price2,
+                purchaseDate: DateTimeOffset.Parse("2023-10-10"),
+                exchangeName: "Binance",
+                transactionType: ETransactionType.Buy)
+        };
+
+        foreach (var transaction in transactions)
+        {
+            cryptoAsset.AddTransaction(transaction);
+        }
+
+        // Assert
+        cryptoAsset.TotalInvested.Should().Be(expectedTotalCost);
     }
 }
