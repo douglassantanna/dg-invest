95837e584dfe4f3e106f508f43b55c64b97b2ea8
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2023-11-27 18:14:24 +0000

    refactored view-cryptos-component layout

--- web-app/src/app/pages/cryptos/components/crypto-card.component.ts
+++ web-app/src/app/pages/cryptos/components/crypto-card.component.ts
@@ -9,22 +9,18 @@ import { ViewMinimalCryptoAssetDto } from 'src/app/core/models/view-minimal-cryp
   standalone: true,
   imports: [CommonModule, DecimalRoundPipe],
   template: `
-
-
-        <div class="card shadow p-3 mb-5 bg-white rounded">
-          <div class="card-body">
-            <h2 class="card-title">{{ crypto.symbol | uppercase }}</h2>
-            <p class="card-text">Price: {{ crypto.currentPrice | currency:'USD' }}</p>
-            <p class="card-text">Percent Change 24h:
-              <span [ngStyle]="{ color: crypto.percentChange24h >= 0 ? 'green' : 'red', 'font-weight': 'bold' }">
-                {{ crypto.percentChange24h | decimalRound }}%
-              </span>
-            </p>
-            <a (click)="cryptoDashboard(crypto.id)" class="btn btn-primary">See details</a>
-          </div>
-        </div>
-
-
+  <div class="card shadow p-3 mb-5 bg-white rounded">
+    <div class="card-body">
+      <h2 class="card-title">{{ crypto.symbol | uppercase }}</h2>
+      <p class="card-text">Price: {{ crypto.currentPrice | currency:'USD' }}</p>
+      <p class="card-text">Percent Change 24h:
+        <span [ngStyle]="{ color: crypto.percentChange24h >= 0 ? 'green' : 'red', 'font-weight': 'bold' }">
+          {{ crypto.percentChange24h | decimalRound }}%
+        </span>
+      </p>
+      <a (click)="cryptoDashboard(crypto.id)" class="btn btn-primary">See details</a>
+    </div>
+  </div>
   `,
 })
 export class CryptoCardComponent {
--- web-app/src/app/pages/cryptos/containers/view-cryptos.component.ts
+++ web-app/src/app/pages/cryptos/containers/view-cryptos.component.ts
@@ -5,7 +5,7 @@ import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CreateCryptoComponent } from './create-crypto.component';
 import { CryptoCardComponent } from '../components/crypto-card.component';
 import { CryptoService } from '../../../core/services/crypto.service';
-import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap, takeUntil } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, debounceTime, distinctUntilChanged, switchMap, takeUntil } from 'rxjs';
 import { ViewMinimalCryptoAssetDto } from 'src/app/core/models/view-minimal-crypto-asset-dto';
 @Component({
   selector: 'app-view-cryptos',
@@ -18,27 +18,38 @@ import { ViewMinimalCryptoAssetDto } from 'src/app/core/models/view-minimal-cryp
     ReactiveFormsModule],
   template: `
     <main class="container">
-      <header class="d-flex justify-content-between align-items-center">
-        <h1>Portfolio</h1>
-        <div>
-          <app-create-crypto (cryptoCreated)="loadCryptoAssets()"></app-create-crypto>
+      <div class="row mt-2">
+        <div class="col-md-6">
+          <h1>Portfolio</h1>
         </div>
-      </header>
-      <div>
-        <div class="row g-3">
-          <div class="col mb-3 position-relative">
-            <input class="form-control" placeholder="Search by name.." aria-label="Search" type="text" [formControl]="searchControl" >
-            <span
-              *ngIf="cryptos.length == 0">
-              No assets found. Add some. 🧳</span>
+
+        <div class="col-md-6">
+          <div class="row">
+            <div class="col">
+              <input class="form-control" placeholder="Search by name.." aria-label="Search" type="text" [formControl]="searchControl">
+              <span *ngIf="cryptos$.value.length === 0" class="text-danger">
+                No assets found. Add some. 🧳
+              </span>
+            </div>
+            <div class="col-auto">
+              <app-create-crypto (cryptoCreated)="loadCryptoAssets()"></app-create-crypto>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="row" >
-        <div class="col-md-4" *ngFor="let crypto of cryptos">
-          <app-crypto-card [crypto]="crypto" />
+      <div class="row">
+        <div *ngIf="cryptos$ | async as cryptos; else loading">
+          <div class="row">
+            <div class="col-md-4" *ngFor="let crypto of cryptos">
+              <app-crypto-card [crypto]="crypto" />
+            </div>
+          </div>
         </div>
+
+        <ng-template #loading>
+          <div class="text-center">Loading...</div>
+        </ng-template>
       </div>
     </main>
   `,
@@ -50,7 +61,7 @@ export class ViewCryptosComponent implements OnInit, OnDestroy {
   private cryptoService = inject(CryptoService);
   private unsubscribe$: Subject<void> = new Subject<void>();
 
-  cryptos: ViewMinimalCryptoAssetDto[] = [];
+  cryptos$: BehaviorSubject<ViewMinimalCryptoAssetDto[]> = new BehaviorSubject<ViewMinimalCryptoAssetDto[]>([]);
   searchControl: FormControl = new FormControl();
   results$!: Observable<any[]>;
 
@@ -73,7 +84,7 @@ export class ViewCryptosComponent implements OnInit, OnDestroy {
     this.cryptoService.getCryptoAssets(page, pageSize, cryptoCurrency, sortOrder)
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe(cryptos => {
-        this.cryptos = cryptos.items;
+        this.cryptos$.next(cryptos.items);
       });
   }
 
@@ -86,7 +97,7 @@ export class ViewCryptosComponent implements OnInit, OnDestroy {
       }),
       takeUntil(this.unsubscribe$)
     ).subscribe((searchResults) => {
-      this.cryptos = searchResults.items;
+      this.cryptos$.next(searchResults.items);
     });
   }
 }
