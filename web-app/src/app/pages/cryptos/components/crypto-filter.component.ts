2372e248f560aba2a2d2ca3aea2c55d4b5524717
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2023-12-14 17:14:04 +0000

    added crypto-component

---
+++ web-app/src/app/pages/cryptos/components/crypto-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-crypto-filter',
+  standalone: true,
+  imports: [CommonModule],
+  template: ``,
+})
+export class CryptoFilterComponent {
+
+}
