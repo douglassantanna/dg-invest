4faa8736d3a34ca20cfd1ba13fb0aed2a4c0c422
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2023-12-27 13:41:11 +0000

    added structured logging using serilog

--- api/api/Authentication/Commands/AuthenticateCommand.cs
+++ api/api/Authentication/Commands/AuthenticateCommand.cs
@@ -21,30 +21,38 @@ public class AuthenticateHandler : IRequestHandler<AuthenticateCommand, Response
     private readonly DataContext _context;
     private readonly ITokenService _tokenService;
     private readonly IPasswordHelper _passwordHelper;
+    private readonly ILogger<AuthenticateHandler> _logger;
 
     public AuthenticateHandler(
         DataContext context,
         ITokenService tokenService,
-        IPasswordHelper passwordHelper)
+        IPasswordHelper passwordHelper,
+        ILogger<AuthenticateHandler> logger)
     {
         _context = context;
         _tokenService = tokenService;
         _passwordHelper = passwordHelper;
+        _logger = logger;
     }
     public async Task<Response> Handle(AuthenticateCommand request, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Authenticating user {0}", request.Email);
+
         var user = await _context.Users.Where(u => u.Email == request.Email).FirstOrDefaultAsync(cancellationToken);
         if (user is null)
         {
+            _logger.LogInformation("User {0} not found", request.Email);
             return new Response("User not found", false);
         }
 
         if (!_passwordHelper.VerifyPassword(request.Password, user.Password ?? string.Empty))
         {
+            _logger.LogInformation("Password for user {0} is incorrect", request.Email);
             return new Response("Password is incorrect", false);
         }
 
         var token = _tokenService.GenerateToken(user);
+        _logger.LogInformation("User {0} authenticated", request.Email);
         return new Response("", true, new { token });
     }
 }
