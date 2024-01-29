3bfcd8fd002099b4dc66ae150ce5e14453c3c4ef
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-01-29 22:54:50 +0000

    updated prod url

--- web-app/src/environments/environment.development.ts
+++ web-app/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://dg-api-w.azurewebsites.net/api',
+  apiUrl: 'https://localhost:7204/api',
 };
 export const local_storage_token = 'dg-invest-token';
