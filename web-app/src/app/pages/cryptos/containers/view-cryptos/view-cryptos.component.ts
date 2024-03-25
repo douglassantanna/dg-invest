2012b02226291218271f68fdc695d5a38f2d9013
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-03-25 11:03:35 +0000

    added view-crypto.component.html

--- web-app/src/app/pages/cryptos/containers/view-cryptos/view-cryptos.component.ts
+++ web-app/src/app/pages/cryptos/containers/view-cryptos/view-cryptos.component.ts
@@ -2,13 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { CryptoService } from '../../../core/services/crypto.service';
+import { CryptoService } from '../../../../core/services/crypto.service';
 import { BehaviorSubject, Observable, Subject, takeUntil } from 'rxjs';
-import { CryptoFilterComponent } from '../components/crypto-filter/crypto-filter.component';
-import { CryptoTableComponent } from '../components/crypto-table/crypto-table.component';
+import { CryptoFilterComponent } from '../../components/crypto-filter/crypto-filter.component';
+import { CryptoTableComponent } from '../../components/crypto-table/crypto-table.component';
 import { ViewCryptoInformation } from 'src/app/core/models/view-crypto-information';
 import { LocalStorageService } from 'src/app/core/services/local-storage.service';
-import { CreateAssetComponent } from '../components/create-asset/create-asset.component';
+import { CreateAssetComponent } from '../../components/create-asset/create-asset.component';
 import { ButtonComponent } from 'src/app/layout/button/button.component';
 import { MatDialog } from '@angular/material/dialog';
 
@@ -23,74 +23,7 @@ import { MatDialog } from '@angular/material/dialog';
     CryptoTableComponent,
     CreateAssetComponent,
     ButtonComponent],
-  template: `
-    <main class="container">
-      <header>
-        <div class="coll-1">
-          <h1>Cryptos</h1>
-        </div>
-
-        <ng-container *ngIf="$emptyCryptoArray | async">
-          <div class="coll-2">
-            <app-crypto-filter
-              (searchControlEvent)="search($event, hideZeroBalance)"
-              (hideZeroBalanceControlEvent)="search('', hideZeroBalance = $event)"
-              [setBalanceStatus]="setBalanceStatus">
-            </app-crypto-filter>
-          </div>
-
-          <div class="coll-3">
-            <app-button [loading]="true" [text]="'Add asset'" (submit)="openModal()"></app-button>
-          </div>
-        </ng-container>
-      </header>
-
-      <div class="row">
-        <div *ngIf="cryptos$ | async as cryptos; else loading">
-          <ng-container *ngIf="cryptos?.length; else emptyCriptoList">
-            <div class="row">
-              <div class="col-md-12">
-                <app-crypto-table [cryptos]="cryptos" [hideZeroBalance]="hideZeroBalance" />
-              </div>
-            </div>
-          </ng-container>
-        </div>
-
-        <ng-template #loading>
-          <div class="text-center">Loading...</div>
-        </ng-template>
-      </div>
-
-      <ng-template #emptyCriptoList>
-        <div class="text-center">
-          <h2>No assets found 😥</h2>
-          <app-button [loading]="true" [text]="'Add asset'" (submit)="openModal()"></app-button>
-        </div>
-      </ng-template>
-
-    </main>
-  `,
-  styles: [`
-    header {
-      display: flex;
-      flex-wrap: wrap;
-    }
-
-    .coll-1,
-    .coll-2,
-    .coll-3 {
-      padding: 5px 0px 5px 5px;
-    }
-    .coll-1{ width: 50%; }
-    .coll-2{ width: 40%; }
-    .coll-3{ width: 10%; display: flex; justify-content: flex-end;}
-
-    @media screen and (max-width: 768px) {
-      .coll-1{ width: 70%; justify-content: space-between; order: 1;}
-      .coll-2{ flex: 1; width: 100%; order: 3; }
-      .coll-3{ width: 30%; order: 2; display: flex; justify-content: flex-end; }
-      }
-`]
+  templateUrl: 'view-cryptos.component.html'
 })
 export class ViewCryptosComponent implements OnInit, OnDestroy {
   private cryptoService = inject(CryptoService);
