1bcd2a624d35cd62c24054d81752f56b79e578e1
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-03-25 16:38:56 +0000

    refactored crypto-table component

--- web-app/src/app/pages/cryptos/components/crypto-table/crypto-table.component.ts
+++ web-app/src/app/pages/cryptos/components/crypto-table/crypto-table.component.ts
@@ -1,27 +1,36 @@
 import { Component, Input, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DecimalPipe } from '@angular/common';
 import { PercentDifferenceComponent } from '../percent-difference.component';
 import { Router } from '@angular/router';
 import { ViewCryptoInformation } from 'src/app/core/models/view-crypto-information';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 @Component({
   selector: 'app-crypto-table',
   standalone: true,
   imports: [
     CommonModule,
-    PercentDifferenceComponent],
-  templateUrl: './crypto-table.component.html',
-  styleUrls: ['./crypto-table.component.scss']
+    PercentDifferenceComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule
+  ],
+  templateUrl: './crypto-table.component.html'
 })
 export class CryptoTableComponent {
   @Input() cryptos: ViewCryptoInformation[] = [];
   @Input() hideZeroBalance: boolean = false;
-  avaragePrice = 5000;
-  balance = 5;
+  displayedColumns: string[] = ['name', 'unitPrice', 'investedAmount', 'currentWorth', 'action'];
+  headerText: string = '';
   router = inject(Router);
-  currentWorth = 7448;
-  gainLoss = 14;
   cryptoDashboard(cryptoID: number) {
     this.router.navigate(['/crypto-dashboard', cryptoID]);
   }
+  decimalPipe = new DecimalPipe('en-US');
+
+  getPrice = (data: ViewCryptoInformation): string => {
+    const result = this.decimalPipe.transform(data.pricePerUnit, '1.0-3');
+    return result === null ? '' : result;
+  };
 }
