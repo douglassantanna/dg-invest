b59a3d281a7533dd834064cfd29aa54ae41ce9b7
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-05-30 16:36:37 +0100

    created pie chart component and separeted view-cryptos html from .ts file

---
+++ web-app/src/app/pages/cryptos/components/pie-chart/pie-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pie-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './pie-chart.component.html',
+})
+export class PieChartComponent {
+
+}
--- web-app/src/app/pages/cryptos/containers/view-cryptos/view-cryptos.component.ts
+++ web-app/src/app/pages/cryptos/containers/view-cryptos/view-cryptos.component.ts
@@ -2,15 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { CreateCryptoComponent } from './create-crypto.component';
-import { CryptoCardComponent } from '../components/crypto-card.component';
-import { CryptoService } from '../../../core/services/crypto.service';
+import { CreateCryptoComponent } from '../create-crypto.component';
+import { CryptoCardComponent } from '../../components/crypto-card.component';
+import { CryptoService } from '../../../../core/services/crypto.service';
 import { BehaviorSubject, Observable, Subject, takeUntil } from 'rxjs';
-import { CryptoFilterComponent } from '../components/crypto-filter.component';
-import { CryptoTableComponent } from '../components/crypto-table/crypto-table.component';
+import { CryptoFilterComponent } from '../../components/crypto-filter.component';
+import { CryptoTableComponent } from '../../components/crypto-table/crypto-table.component';
 import { ViewCryptoInformation } from 'src/app/core/models/view-crypto-information';
 import { LocalStorageService } from 'src/app/core/services/local-storage.service';
-import { PercentDifferenceComponent } from '../components/percent-difference.component';
+import { PercentDifferenceComponent } from '../../components/percent-difference.component';
+import { PieChartComponent } from '../../components/pie-chart/pie-chart.component';
 
 @Component({
   selector: 'app-view-cryptos',
@@ -23,99 +24,9 @@ import { PercentDifferenceComponent } from '../components/percent-difference.com
     ReactiveFormsModule,
     CryptoFilterComponent,
     CryptoTableComponent,
-    PercentDifferenceComponent],
-  template: `
-    <main class="container">
-      <header>
-        <div class="coll-1">
-          <h1>Cryptos</h1>
-        </div>
-
-        <ng-container *ngIf="$emptyCryptoArray | async">
-          <div class="coll-2">
-            <app-crypto-filter
-            (viewDataTableEvent)="displayDataTableView($event)"
-            (searchControlEvent)="search($event, hideZeroBalance)"
-            (hideZeroBalanceControlEvent)="search('', hideZeroBalance = $event)"
-            [setBalanceStatus]="setBalanceStatus">
-          </app-crypto-filter>
-        </div>
-
-        <div class="coll-3">
-          <app-create-crypto (cryptoCreated)="loadCryptoAssets()"></app-create-crypto>
-        </div>
-      </ng-container>
-    </header>
-
-    <div class="row">
-      <div class="container mt-3">
-        <div class="row">
-          <div class="col-md-6">
-          <div class="alert alert-light">
-            <strong>Total Invested:</strong> {{ totalInvested | currency:'USD':'symbol':'1.2-2'}}
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="alert alert-light">
-            <strong>Total Market Value:</strong> {{ totalMarketValue | currency:'USD':'symbol':'1.2-2'}}
-            <app-percent-difference [percentDifference]="investmentChangePercent"></app-percent-difference>
-          </div>
-        </div>
-      </div>
-      </div>
-
-        <div *ngIf="cryptos$ | async as cryptos; else loading">
-          <ng-container *ngIf="cryptos?.length; else emptyCriptoList">
-            <ng-template #cardView>
-              <div class="row">
-                <div class="col-md-4" *ngFor="let crypto of cryptos">
-                  <app-crypto-card [crypto]="crypto" />
-                </div>
-              </div>
-            </ng-template>
-            <div class="row" *ngIf="displayDataTable;else cardView">
-              <div class="col-md-12">
-                <app-crypto-table [cryptos]="cryptos" [hideZeroBalance]="hideZeroBalance" />
-              </div>
-            </div>
-          </ng-container>
-        </div>
-
-        <ng-template #loading>
-          <div class="text-center">Loading...</div>
-        </ng-template>
-      </div>
-
-      <ng-template #emptyCriptoList>
-        <div class="text-center">
-          <h2>No assets found 😥</h2>
-          <app-create-crypto (cryptoCreated)="loadCryptoAssets()"></app-create-crypto>
-        </div>
-      </ng-template>
-
-    </main>
-  `,
-  styles: [`
-    header {
-      display: flex;
-      flex-wrap: wrap;
-    }
-
-    .coll-1,
-    .coll-2,
-    .coll-3 {
-      padding: 5px 0px 5px 5px;
-    }
-    .coll-1{ width: 50%; }
-    .coll-2{ width: 40%; }
-    .coll-3{ width: 10%; display: flex; justify-content: flex-end;}
-
-    @media screen and (max-width: 768px) {
-      .coll-1{ width: 70%; justify-content: space-between; order: 1;}
-      .coll-2{ flex: 1; width: 100%; order: 3; }
-      .coll-3{ width: 30%; order: 2; display: flex; justify-content: flex-end; }
-      }
-`]
+    PercentDifferenceComponent,
+    PieChartComponent],
+  templateUrl: 'view-cryptos.component.html'
 })
 export class ViewCryptosComponent implements OnInit, OnDestroy {
   private cryptoService = inject(CryptoService);
