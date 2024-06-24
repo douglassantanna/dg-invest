456c2cd3ad14aa6782cc0cb3ab792daabb8be458
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-06-24 22:38:19 +0100

    display user information in my-user-profile.component

--- web-app/src/app/pages/users/container/user-profile/user-profile.component.ts
+++ web-app/src/app/pages/users/container/user-profile/user-profile.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { UserDecode } from 'src/app/core/models/user-decode';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -18,6 +20,21 @@ import { MatInputModule } from '@angular/material/input';
     FormsModule],
   templateUrl: './user-profile.component.html'
 })
-export class UserProfileComponent {
-  loading = true;
+export class UserProfileComponent implements OnInit {
+  private authService = inject(AuthService);
+  loading = false;
+  userFullname = '';
+  userEmail = '';
+
+  ngOnInit(): void {
+    if (this.authService.user) {
+      this.userFullname = this.authService.user.unique_name;
+      this.userEmail = this.authService.user.email;
+    }
+  }
+
+  updateUserProfile() {
+    console.log(this.userFullname)
+    console.log(this.userEmail)
+  }
 }
