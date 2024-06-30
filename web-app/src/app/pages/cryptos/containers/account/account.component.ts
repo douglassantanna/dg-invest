75b4bfdae59c8d42293f8279c7822be4d559f6aa
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-06-30 16:57:44 +0100

    create recent transactions section in account-component

--- web-app/src/app/pages/cryptos/containers/account/account.component.ts
+++ web-app/src/app/pages/cryptos/containers/account/account.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CryptoFilterComponent } from '../../components/crypto-filter.component';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [],
+  imports: [
+    CryptoFilterComponent
+  ],
   templateUrl: './account.component.html',
 })
 export class AccountComponent {
 
+  searchTransactions(input: any) {
+    console.log(input);
+  }
 }
