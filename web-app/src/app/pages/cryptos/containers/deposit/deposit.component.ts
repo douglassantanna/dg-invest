08d2041d32613f497f160b7f684fcc0ff4dea173
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-07-01 23:08:02 +0100

    style deposit component

--- web-app/src/app/pages/cryptos/containers/deposit/deposit.component.ts
+++ web-app/src/app/pages/cryptos/containers/deposit/deposit.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+export type Deposity = {
+  type: number;
+  amount: number;
+  currentPrice: number;
+  exchangeName: string;
+  date: Date;
+}
 @Component({
   selector: 'app-deposit',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './deposit.component.html',
   styleUrl: './deposit.component.scss'
 })
-export class DepositComponent {
+export class DepositComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  deposityForm = this.fb.group({
+    type: [0],
+    amount: ['', [Validators.min(0)]],
+    currentPrice: ['',],
+    exchangeName: [''],
+    date: [''],
+  })
 
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.deposityForm.value)
+  }
 }
