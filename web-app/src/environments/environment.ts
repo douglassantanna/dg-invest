720b15c6337164126ec210285ff5c930963caa4f
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-08-11 18:54:20 +0100

    replace api url in environment file

--- web-app/src/environments/environment.ts
+++ web-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dg-api-w.azurewebsites.net/api',
+  apiUrl: 'http://localhost:8080/api',
 };
 export const local_storage_token = 'dg-invest-token';
