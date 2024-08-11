6ae2b263899c31dc9d92bc9e193785ccded4ba08
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-08-11 20:05:19 +0100

    create ServiceExtensions class

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -1,70 +1,21 @@
 using System.Reflection;
 using System.Text.Json.Serialization;
-using api.Authentication;
-using api.AzureStorage;
-using api.AzureStorage.Queue;
-using api.CoinMarketCap;
-using api.CoinMarketCap.Service;
-using api.Cryptos.Repositories;
-using api.Cryptos.TransactionStrategies;
-using api.Cryptos.TransactionStrategies.Contracts;
-using api.Cryptos.TransactionStrategies.Transactions;
 using api.Data;
-using api.Data.Repositories;
-using api.Interfaces;
-using api.RateLimiterPolicies;
 using api.Shared;
-using api.Users.Repositories;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IPasswordHelper, PasswordHelper>();
-builder.Services.AddScoped<ICoinMarketCapService, CoinMarketCapService>();
-builder.Services.AddScoped<IQueueService, QueueService>();
-
-builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(RepositoryBase<>));
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<ICryptoRepository, CryptoRepository>();
-builder.Services.AddScoped<ICryptoAssetRepository, CryptoAssetRepository>();
-builder.Services.AddScoped<ITransactionStrategy, BuyTransaction>();
-builder.Services.AddScoped<ITransactionStrategy, SellTransaction>();
-builder.Services.AddScoped<ITransactionStrategy, FiatDepositTransaction>();
-builder.Services.AddScoped<ITransactionStrategy, WithdrawDepositTransaction>();
-builder.Services.AddScoped<ITransactionStrategy, CryptoDepositTransaction>();
-builder.Services.AddScoped<ITransactionService, TransactionService>();
-
-builder.Services.Configure<CoinMarketCapSettings>(builder.Configuration.GetSection(nameof(CoinMarketCapSettings)));
-builder.Services.Configure<AzureStorageSettings>(builder.Configuration.GetSection(nameof(AzureStorageSettings)));
-
 builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-    x => x.EnableRetryOnFailure());
-});
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Policy",
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
-});
-
-builder.Host.UseSerilog((context, config) =>
-    config
-    .ReadFrom.Configuration(context.Configuration));
-
-builder.Services.AddTokenService(builder.Configuration);
-
-builder.Services.AddCustomRateLimiter(builder.Configuration);
-
+builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
+builder.Services.ConfigureJwt(builder.Configuration);
+builder.Services.ConfigureOptions(builder.Configuration);
+builder.Services.ConfigureServices(builder.Configuration);
+builder.Services.ConfigureDatabase(builder.Configuration);
+builder.Services.ConfigureCustomRateLimiter(builder.Configuration);
+builder.Services.ConfigureCORS();
 builder.Services.AddControllers(opt =>
 {
     opt.Filters.Add<GlobalExceptionFilter>();
@@ -73,9 +24,7 @@ builder.Services.AddControllers(opt =>
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
 });
-
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
---
+++ api/api/Shared/ServiceExtensions.cs
@@ -0,0 +1,129 @@
+using System.Text;
+using System.Threading.RateLimiting;
+using api.Authentication;
+using api.AzureStorage;
+using api.AzureStorage.Queue;
+using api.CoinMarketCap;
+using api.CoinMarketCap.Service;
+using api.Cryptos.Repositories;
+using api.Cryptos.TransactionStrategies;
+using api.Cryptos.TransactionStrategies.Contracts;
+using api.Cryptos.TransactionStrategies.Transactions;
+using api.Data;
+using api.Data.Repositories;
+using api.Interfaces;
+using api.RateLimiterPolicies;
+using api.Users.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace api.Shared;
+public static class ServiceExtensions
+{
+    public const string DefaultPolicy = "DefaultPolicy";
+    public static IServiceCollection ConfigureJwt(this IServiceCollection services, IConfiguration config)
+    {
+        var jwtSettings = config.GetSection(nameof(JWTSettings)).Get<JWTSettings>();
+        var key = Encoding.ASCII.GetBytes(jwtSettings.Secret);
+        services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = null,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+        return services;
+    }
+    public static IServiceCollection ConfigureOptions(this IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<JWTSettings>(config.GetSection(nameof(JWTSettings)));
+        services.Configure<CoinMarketCapSettings>(config.GetSection(nameof(CoinMarketCapSettings)));
+        services.Configure<AzureStorageSettings>(config.GetSection(nameof(AzureStorageSettings)));
+        services.Configure<RateLimiterSettings>(config.GetSection(nameof(RateLimiterSettings)));
+        return services;
+    }
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddScoped<IPasswordHelper, PasswordHelper>();
+        services.AddScoped<ICoinMarketCapService, CoinMarketCapService>();
+        services.AddScoped<IQueueService, QueueService>();
+        services.AddSingleton<ITokenService, TokenService>();
+
+        services.AddScoped(typeof(IBaseRepository<>), typeof(RepositoryBase<>));
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ICryptoRepository, CryptoRepository>();
+        services.AddScoped<ICryptoAssetRepository, CryptoAssetRepository>();
+
+        services.AddScoped<ITransactionService, TransactionService>();
+        services.AddScoped<ITransactionStrategy, BuyTransaction>();
+        services.AddScoped<ITransactionStrategy, SellTransaction>();
+        services.AddScoped<ITransactionStrategy, FiatDepositTransaction>();
+        services.AddScoped<ITransactionStrategy, WithdrawDepositTransaction>();
+        services.AddScoped<ITransactionStrategy, CryptoDepositTransaction>();
+        return services;
+    }
+    public static IServiceCollection ConfigureDatabase(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<DataContext>(options =>
+        {
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection"),
+            x => x.EnableRetryOnFailure());
+        });
+        return services;
+    }
+    public static IServiceCollection ConfigureCORS(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("Policy",
+                policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+        });
+        return services;
+    }
+    public static IServiceCollection ConfigureCustomRateLimiter(this IServiceCollection services, IConfiguration config)
+    {
+        var rateLimiterSettings = config.GetSection(nameof(RateLimiterSettings)).Get<RateLimiterSettings>();
+
+        if (!int.TryParse(rateLimiterSettings.RequestsPermitLimit, out var permitLimit))
+        {
+            permitLimit = 320;
+        }
+
+        if (!int.TryParse(rateLimiterSettings.WindowLimitInMinutes, out var windowLimitInMinutes))
+        {
+            windowLimitInMinutes = 1440;
+        }
+
+        services.AddRateLimiter(options =>
+        {
+            options.AddFixedWindowLimiter(policyName: DefaultPolicy, options =>
+            {
+                options.PermitLimit = permitLimit;
+                options.Window = TimeSpan.FromMinutes(windowLimitInMinutes);
+                options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                options.QueueLimit = 0;
+            });
+            options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+        });
+        return services;
+    }
+}
