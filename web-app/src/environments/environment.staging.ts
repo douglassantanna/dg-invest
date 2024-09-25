1c6fb83dfbe4f668d7414205776c378387cc127f
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-09-25 12:51:32 +0100

    Update environment.staging.ts

--- web-app/src/environments/environment.staging.ts
+++ web-app/src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'https://dgi-api-w-dzhgetg4g7hab0fp.northeurope-01.azurewebsites.net/api',
 };
 export const local_storage_token = 'dg-invest-token';
