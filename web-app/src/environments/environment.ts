3e7f778f280135587a7d3084f77f3c7330eb7e5a
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-10-21 21:47:32 +0100

    update theme color

--- web-app/src/environments/environment.ts
+++ web-app/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://dg-api-w.azurewebsites.net/api',
-  navbarColor: 'bg-primary',
-  btnColor: 'btn-primary',
+  navbarColor: 'bg-dark',
+  btnColor: 'btn-dark',
 };
 export const local_storage_token = 'dg-invest-token';
