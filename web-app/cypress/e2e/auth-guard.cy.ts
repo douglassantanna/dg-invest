3e79cda12c4622a318e5be6896ba8df56540430f
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-10-25 12:51:26 +0100

    create invalid fake token

--- web-app/cypress/e2e/auth-guard.cy.ts
+++ web-app/cypress/e2e/auth-guard.cy.ts
@@ -1,7 +1,7 @@
 describe('Auth Guard', () => {
   const localStorageTokenKey = 'dg-invest-token';
-  const fakeJwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG45QGVtYWlsLmNvbSIsIm5hbWVpZCI6IjIwMDgiLCJ1bmlxdWVfbmFtZSI6IkpvaG4gRG9lIiwicm9sZSI6IkFkbWluIiwibmJmIjoxNzI5ODU0MjU2LCJleHAiOjE3MzA0NTkwNTYsImlhdCI6MTcyOTg1NDI1NiwiaXNzIjoiaHR0cDovL215LWxvY2FsLWhvc3QuY29tIn0.eqzUT6DYqXcftzsIY-XDToI578_biUWE1DrVbp2PhYQ';
-
+  const fakeValidJwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG45QGVtYWlsLmNvbSIsIm5hbWVpZCI6IjIwMDgiLCJ1bmlxdWVfbmFtZSI6IkpvaG4gRG9lIiwicm9sZSI6IkFkbWluIiwibmJmIjoxNzI5ODU0MjU2LCJleHAiOjE3MzA0NTkwNTYsImlhdCI6MTcyOTg1NDI1NiwiaXNzIjoiaHR0cDovL215LWxvY2FsLWhvc3QuY29tIn0.eqzUT6DYqXcftzsIY-XDToI578_biUWE1DrVbp2PhYQ';
+  const fakeExpiredJwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.QuY29tIn0';
   beforeEach(() => {
     cy.clearLocalStorage();
   });
@@ -20,11 +20,15 @@ describe('Auth Guard', () => {
   it('should redirect users with an invalid JWT to the login page', () => {
     // Set an invalid JWT using the LocalStorageService's setToken method
     cy.window().then((window) => {
-      window.localStorage.setItem(localStorageTokenKey, JSON.stringify({ jwtToken: 'invalid-jwt-token' }));
+      window.localStorage.setItem(localStorageTokenKey, JSON.stringify({ jwtToken: fakeExpiredJwt }));
     });
 
     // Visit the cryptos page
     cy.visit('http://localhost:4200/#/cryptos');
+
+    cy.url().then((url) => {
+      cy.log('Current URL:', url);
+    });
 
     // Check if the URL includes 'login'
     cy.url({ timeout: 10000 }).should('include', '/login');
@@ -36,7 +40,7 @@ describe('Auth Guard', () => {
   it('should allow authenticated users to access the cryptos page', () => {
     // Set a valid JWT using the LocalStorageService's setToken method
     cy.window().then((window) => {
-      window.localStorage.setItem(localStorageTokenKey, JSON.stringify({ jwtToken: fakeJwt }));
+      window.localStorage.setItem(localStorageTokenKey, JSON.stringify({ jwtToken: fakeValidJwt }));
     });
 
     // Visit the cryptos page
