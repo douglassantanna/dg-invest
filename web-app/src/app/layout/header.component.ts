87f36e2ad38152bf3c31fe1bfdb8335fd3c257de
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-10-28 15:41:59 +0000

    update header component to display username initial name letter and welcome message

--- web-app/src/app/layout/header.component.ts
+++ web-app/src/app/layout/header.component.ts
@@ -1,5 +1,5 @@
 import { Role } from './../core/models/user.model';
-import { NgClass } from '@angular/common';
+import { NgClass, SlicePipe } from '@angular/common';
 import { Component, computed, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
@@ -13,20 +13,24 @@ import { environment } from 'src/environments/environment.development';
   standalone: true,
   imports: [
     NgClass,
-    RouterModule],
+    RouterModule,
+    SlicePipe],
   template: `
     <nav class="navbar navbar-expand-lg" [ngClass]="navbarColor" data-bs-theme="dark">
       <div class="container-fluid">
-        <a class="navbar-brand">DG</a>
+        <a class="navbar-brand" style=" border: white 1px solid; padding:5px; border-radius:50px; color:white; width:30px; height:30px; display:flex; align-items:center; justify-content:center">{{ username() | slice: 0: 1 }}</a>
+
         <button class="navbar-toggler" type="button" (click)="toggleMenu()" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" [ngClass]="{'collapse': isCollapsed()}" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            @for (item of navItemsEnabled(); track $index) {
-              <a class="nav-link" aria-current="page" [routerLink]="item.path" (click)="logout(item)">{{ item.label }}</a>
-            }
+          @for (item of navItemsEnabled(); track $index) {
+                  <a class="nav-link" aria-current="page" [routerLink]="item.path" (click)="logout(item)">{{ item.label }}</a>
+                }
           </div>
+          <span class="navbar-text ms-auto me-3">Welcome, <b>{{ username() }}</b></span>
         </div>
       </div>
     </nav>
@@ -44,6 +48,7 @@ export class HeaderComponent {
     return filteredNavItems;
   }
   );
+  username = computed(() => this.authService.user?.unique_name);
 
   toggleMenu() {
     this.isCollapsed.set(!this.isCollapsed());
