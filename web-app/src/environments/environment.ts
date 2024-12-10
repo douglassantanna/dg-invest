8458b3e2ce2ce7f9e9282f8bae80d7fab358c8f5
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-12-10 00:15:43 +0000

    add btn color value environment file

--- web-app/src/environments/environment.ts
+++ web-app/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:8080/api',
+  navbarColor: 'bg-secondary',
+  btnColor: 'btn-secondary',
 };
 export const local_storage_token = 'dg-invest-token';
