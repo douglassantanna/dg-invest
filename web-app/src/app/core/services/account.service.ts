e4823fc149369f37e8e3d9b9a531799a8b6508b7
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-12-13 19:45:13 +0000

    create AccountService

---
+++ web-app/src/app/core/services/account.service.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { CustomRespose } from '../models/custom-response';
+
+export interface CreateAccountCommand {
+  subaccountTag: string;
+}
+
+export interface AddCryptoAssetRequest {
+  cryptoId: number;
+}
+
+export interface AddTransactionCommand {
+  amount: number;
+  price: number;
+  purchaseDate: Date;
+  exchangeName: string;
+  transactionType: string;
+  cryptoAssetId: number;
+  fee: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private readonly apiUrl = `${environment.apiUrl}/Account`;
+  private readonly http = inject(HttpClient);
+
+  getAccounts(): Observable<CustomRespose> {
+    return this.http.get<CustomRespose>(`${this.apiUrl}`);
+  }
+
+  createAccount(command: CreateAccountCommand): Observable<CustomRespose> {
+    return this.http.post<CustomRespose>(`${this.apiUrl}/create`, command);
+  }
+
+  getAccountBySubAccountTag(subAccountTag: string): Observable<CustomRespose> {
+    return this.http.get<CustomRespose>(`${this.apiUrl}/${subAccountTag}`);
+  }
+
+  addCryptoAsset(subAccountTag: string, request: AddCryptoAssetRequest): Observable<CustomRespose> {
+    return this.http.post<CustomRespose>(
+      `${this.apiUrl}/${subAccountTag}/add-crypto-asset`,
+      request
+    );
+  }
+
+  addTransaction(subAccountTag: string, command: AddTransactionCommand): Observable<CustomRespose> {
+    return this.http.post<CustomRespose>(
+      `${this.apiUrl}/${subAccountTag}/add-transaction`,
+      command
+    );
+  }
+
+}
