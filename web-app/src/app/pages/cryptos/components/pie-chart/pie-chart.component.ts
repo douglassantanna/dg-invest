728b4f9f74566c4808cbe31c280b64967452900b
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2025-02-11 00:08:28 +0000

    refactor pie-chart.component; use echarts instead of chartjs

--- web-app/src/app/pages/cryptos/components/pie-chart/pie-chart.component.ts
+++ web-app/src/app/pages/cryptos/components/pie-chart/pie-chart.component.ts
@@ -1,128 +1,107 @@
-import { Component, SimpleChanges, input, signal } from '@angular/core';
-import { Chart, registerables } from 'chart.js';
-Chart.register(...registerables);
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild, computed, input, signal } from '@angular/core';
 import { ViewCryptoInformation } from 'src/app/core/models/view-crypto-information';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { forEach } from 'cypress/types/lodash';
 import { NgStyle } from '@angular/common';
+import * as echarts from 'echarts';
+
 @Component({
   selector: 'app-pie-chart',
   standalone: true,
   imports: [NgStyle],
   templateUrl: './pie-chart.component.html',
 })
-export class PieChartComponent {
+export class PieChartComponent implements AfterViewInit {
   cryptos = input<ViewCryptoInformation[]>([]);
-  labels: string[] = [];
-  imagesAndColors: string[] = [];
-  positionValues: number[] = [];
-  cryptoImages = signal<CryptoImageAux[]>([]);
+  @ViewChild('pieChartContainer', { static: false }) pieChartContainer!: ElementRef;
+  pieChartInstance: any = null;
+  pieChartTitle = signal('Portfolio Holdings');
+  isMobileMode = computed(() => window.innerWidth < 640);
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (this.pieChartInstance) {
+      this.pieChartInstance.resize();
+    }
+  }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['cryptos']) {
-      const cryptos = changes['cryptos'].currentValue;
-      this.imagesAndColors = cryptos.map((x: any) => {
-        let cryptoSymbol = x.symbol.replace(/USD$/, '');
-        return `https://dgistage.blob.core.windows.net/crypto-logos/${cryptoSymbol}-logo.png`;
-      });
-      this.labels = cryptos.map((x: any) => x.symbol);
-      this.positionValues = cryptos.map((x: any) => x.currentWorth);
+  ngAfterViewInit(): void {
+    this.initPieChart();
+  }
 
-      const ctx = document.getElementById('chart') as HTMLCanvasElement;
-      if (Chart.getChart(ctx)) {
-        Chart.getChart(ctx)?.destroy();
+  private initPieChart() {
+    const chartElement = this.pieChartContainer.nativeElement;
+    if (this.pieChartInstance) {
+      this.pieChartInstance.dispose();
+    }
+    this.pieChartInstance = echarts.init(chartElement);
+    const totalSum = this.cryptos().reduce((sum, item) => sum + item.investedAmount, 0);
+    const cryptoHoldingData = this.cryptos().map((item) => {
+      return {
+        value: item.investedAmount,
+        name: item.symbol
       }
+    });
 
-      const colors = this.generateColors(this.imagesAndColors);
-
-      const centerTextPlugin = {
-        id: 'centerText',
-        beforeDraw: (chart: any) => {
-          const { width, height, ctx } = chart;
-          ctx.save();
-          ctx.fillStyle = "#333";
-          ctx.textAlign = "center";
-          ctx.textBaseline = "middle";
-
-          const totalValue = this.positionValues.reduce((sum, val) => sum + val, 0);
-          const displayText = `$${totalValue.toLocaleString("en-US", { minimumFractionDigits: 2 })}`;
-
-          ctx.fillText("Market Value", width / 2, height / 2 - 18);
-          ctx.font = "bold 16px Arial";
-          ctx.fillText(displayText, width / 2, height / 2);
-          ctx.restore();
+    const option = {
+      tooltip: {
+        trigger: 'item',
+        formatter: (params: any) => `${params.name}: $${params.value.toFixed(2)} (${params.percent}%)`,
+        showContent: true
+      },
+      legend: {
+        show: false
+      },
+      avoidLabelOverlap: false,
+      graphic: {
+        type: 'text',
+        left: 'center',
+        top: 'center',
+        style: {
+          text: `Total: ${totalSum.toFixed(2)}`,
+          fontSize: this.isMobileMode() ? 10 : 18,
+          fontWeight: 'bold',
+          fill: '#333'
         }
-      };
-
-      new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-          labels: this.labels,
-          datasets: [{
-            data: this.positionValues,
-            backgroundColor: colors,
-            hoverOffset: 4
-          }]
-        },
-        plugins: [ChartDataLabels, centerTextPlugin],
-        options: {
-          locale: "en-US",
-          responsive: true,
-          maintainAspectRatio: false,
-          plugins: {
-            legend: { display: false },
-            tooltip: {
-              callbacks: {
-                label: function (tooltipItem: any) {
-                  const rawValue = tooltipItem.raw;
-                  const value = parseFloat(rawValue);
-
-                  if (isNaN(value)) {
-                    return rawValue;
-                  }
-
-                  return `${value.toLocaleString("en-US", {
-                    style: "currency",
-                    currency: "USD",
-                    minimumFractionDigits: 2
-                  })}`;
-                }
+      },
+      series: [
+        {
+          name: '',
+          type: 'pie',
+          radius: this.isMobileMode() ? ['40%', '60%'] : ['50%', '70%'],
+          itemStyle: {
+            borderRadius: 5,
+            borderColor: '#fff',
+            borderWidth: 0.5
+          },
+          labelLine: {
+            length: this.isMobileMode() ? 15 : 25,
+          },
+          label: {
+            formatter: (params: any) => `{b|${params.name}}: $${params.value.toFixed(2)} (${params.percent}%)`,
+            backgroundColor: '#F6F8FC',
+            borderColor: '#8C8D8E',
+            rich: {
+              b: {
+                color: '#4C5058',
+                fontSize: this.isMobileMode() ? 12 : 14,
+                fontWeight: 'bold',
+                lineHeight: 33
+              },
+              per: {
+                color: '#fff',
+                backgroundColor: '#4C5058',
+                padding: [3, 4],
+                borderRadius: 4
               }
             },
-            datalabels: {
-              align: 'center',
-              color: 'white',
-              formatter: (value, context) => {
-                const dataPoints = context.chart.config.data.datasets[0].data;
-                function totalSum(total: any, datapoint: any) {
-                  return total + datapoint;
-                }
-                const totalValue = dataPoints.reduce(totalSum, 0);
-                const percentageValue = (value / totalValue * 100).toFixed(1);
-                return `${percentageValue}%`
-              }
-            }
-          }
+            show: this.isMobileMode() ? true : true
+          },
+          data: cryptoHoldingData
         }
-      });
-    }
-  }
-
-  private generateColors(imageUrls: string[]): string[] {
-    const colors = [];
-    for (let i = 0; i < imageUrls.length; i++) {
-      const r = Math.floor(Math.random() * 256);
-      const g = Math.floor(Math.random() * 256);
-      const b = Math.floor(Math.random() * 256);
-
-      const color = `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
-      colors.push(color);
-      this.cryptoImages.set([...this.cryptoImages(), { imageUrl: imageUrls[i], color: color }]);
-    }
-    return colors;
+      ]
+    };
+    this.pieChartInstance.setOption(option);
   }
 }
-export interface CryptoImageAux {
-  imageUrl: string;
-  color: string;
+export interface DataValue {
+  value: number;
+  name: string;
 }
