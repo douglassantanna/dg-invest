dd5dfa612a96b15c963a6f947236a686bc0e4ee8
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2025-02-24 23:23:53 +0000

    🗃️ remove market data point entity and configuration

--- api/api/Data/DataContext.cs
+++ api/api/Data/DataContext.cs
@@ -17,9 +17,7 @@ public class DataContext : DbContext
     public virtual DbSet<User> Users { get; set; } = null!;
     public virtual DbSet<Account> Accounts { get; set; } = null!;
     public virtual DbSet<AccountTransaction> AccountTransactions { get; set; } = null!;
-    public virtual DbSet<MarketDataPoint> MarketDataPoint { get; set; } = null!;
     public virtual DbSet<UserPortfolioSnapshot> UserPortfolioSnapshots { get; set; } = null!;
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
--- api/api/Data/ModelsConfigurations/UserPortfolioSnapshotConfiguration.cs
+++ api/api/Data/ModelsConfigurations/UserPortfolioSnapshotConfiguration.cs
@@ -4,28 +4,6 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace api.Data.ModelsConfigurations;
 
-public class MarketDataPointConfiguration : IEntityTypeConfiguration<MarketDataPoint>
-{
-    public void Configure(EntityTypeBuilder<MarketDataPoint> builder)
-    {
-        builder.HasKey(x => x.Id);
-
-        builder.HasIndex(x => x.UserId);
-        builder.HasIndex(x => x.AccountId);
-        builder.HasIndex(x => x.Time);
-
-        builder.Property(x => x.CoinSymbol)
-            .IsRequired()
-            .HasMaxLength(20);
-
-        builder.Property(x => x.CoinPrice)
-            .HasPrecision(18, 8);
-
-        builder.Property(x => x.Time)
-            .IsRequired();
-    }
-}
-
 public class UserPortfolioSnapshotConfiguration : IEntityTypeConfiguration<UserPortfolioSnapshot>
 {
     public void Configure(EntityTypeBuilder<UserPortfolioSnapshot> builder)
