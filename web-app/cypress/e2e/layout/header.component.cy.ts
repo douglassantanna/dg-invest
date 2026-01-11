3830c1a66bc622d7405c38d31c7e445e3425b97d
Douglas SantAnna <douglbb1@gmail.com>
2026-01-11 19:30:55 +0000

    refactor(header): streamline authentication setup in header component tests

--- web-app/cypress/e2e/layout/header.component.cy.ts
+++ web-app/cypress/e2e/layout/header.component.cy.ts
@@ -1,63 +1,49 @@
 describe('Header Component', () => {
   const localStorageTokenKey = 'dg-invest-token';
-  const fakeValidUserJwt = Cypress.env('FAKE_VALID_USER_JWT');
-  const fakeValidAdminJwt = Cypress.env('FAKE_VALID_JWT');
+  const AUTH_TOKEN = 'auth-token';
 
   beforeEach(() => {
     cy.clearLocalStorage();
   });
 
-  const loginAndSetToken = (token: string) => {
-    cy.intercept('POST', 'https://localhost:7204/api/Authentication/login', {
-      statusCode: 200,
-      body: {
-        data: {
-          token: token
-        }
-      }
-    }).as('loginRequest');
-
-    cy.visit('http://localhost:4200/#/login');
-
-    cy.get('input[formControlName="email"]').type('testuser@example.com');
-    cy.get('input[formControlName="password"]').type('password');
-    cy.get('button[type="submit"]').click();
-
-    cy.wait('@loginRequest');
-
-    cy.window().then((window) => {
-      window.localStorage.setItem(localStorageTokenKey, JSON.stringify({ jwtToken: token }));
+  const setAuthToken = () => {
+    cy.window().then(win => {
+      win.localStorage.setItem(
+        localStorageTokenKey,
+        JSON.stringify({ jwtToken: AUTH_TOKEN })
+      );
     });
   };
 
   const interceptListAssets = () => {
-    cy.intercept('GET', 'https://localhost:7204/api/Crypto/list-assets*', {
+    cy.intercept('GET', '**/api/Crypto/list-assets*', {
       statusCode: 200,
       fixture: 'list-assets.json'
     }).as('listAssets');
   };
 
-  it('should display hamburguer icon in header component', () => {
+  const visitCryptosAsAuthenticatedUser = () => {
     interceptListAssets();
-    loginAndSetToken(fakeValidUserJwt);
+    setAuthToken();
 
-    cy.visit('http://localhost:4200/#/cryptos');
+    cy.visit('/#/cryptos');
+
     cy.wait('@listAssets');
-    cy.url().should('include', '/cryptos');
     cy.get('app-view-cryptos').should('exist');
+  };
 
-    cy.get('app-header header button .material-icons').should('contain', 'menu');
+  it('should display hamburger icon in header component', () => {
+    visitCryptosAsAuthenticatedUser();
+
+    cy.get('app-header header button .material-icons')
+      .should('contain', 'menu');
   });
 
   it('should display username initial letter in header component', () => {
-    interceptListAssets();
-    loginAndSetToken(fakeValidUserJwt);
+    visitCryptosAsAuthenticatedUser();
 
-    cy.visit('http://localhost:4200/#/cryptos');
-    cy.wait('@listAssets');
-    cy.url().should('include', '/cryptos');
-    cy.get('app-view-cryptos').should('exist');
-
-    cy.get('app-header header #username-initial-letter').should('contain', 'd');
+    cy.get('#username-initial-letter')
+      .should('be.visible')
+      .and('not.be.empty');
   });
 });
