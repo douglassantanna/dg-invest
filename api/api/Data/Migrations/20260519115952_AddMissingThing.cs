04089942de2eeb350a551662b5f06ee317b5e00e
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2026-05-19 13:08:25 +0100

    run ef migration to update database with missing fields

---
+++ api/api/Data/Migrations/20260519115952_AddMissingThing.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingThing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExchangeOrderId",
+                table: "CryptoTransactions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BybitUid",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExchangeOrderId",
+                table: "CryptoTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "BybitUid",
+                table: "Accounts");
+        }
+    }
+}
